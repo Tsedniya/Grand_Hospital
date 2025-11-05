19eabe91854392cef04892199dfb686a19197a3c
Tsedniya <ktsedniya@gmail.com>
2025-11-05 23:46:19 +0300

    frontend:about

--- myapp/src/components/TitleAbout.jsx
+++ myapp/src/components/TitleAbout.jsx
@@ -3,9 +3,29 @@ import React from 'react'
 const About = () => {
   return (
     <div >
+      <div className='max-w-8xl'>
         <p className='text-center mt-35 text-6xl text-sky-400'>About</p>
-        <p className="text-center mt-2 mx-55 mb-4">Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.</p>
+        <p className="text-center text-gray-700 mt-2 mx-65 mb-4">Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach.</p>
+      </div>
+        
+      {/*right*/}
+      <div className='flex-col ml-20 mt-14  '>
+        <p >Commited to Excellence in HealthCare</p>
+        <div className='max-w-2xl '>
+            <p className='mt-3 text-gray-700 text-justify leading-relaxed'>At Grand Bishoftu Hospital, we are committed to providing exceptional healthcare with compassion and expertise. Our experienced medical specialists work tirelessly to ensure the well-being of every patient, offering advanced treatments, personalized care, and a patient-first approach. </p>
+            <p className='mt-3  text-gray-700 text-justify leading-relaxed'>We strive to create a safe and welcoming environment where health, comfort, and trust come first.Providing trusted healthcare services with compassion and excellence. Our experienced medical specialists are committed to ensuring the well-being of every patient through advanced treatment, personalized care, and a patient-first approach</p>
+        </div>
+        </div>
+           
+        {/*left*/}
+        <div>
+           <img/>
+        </div>
+        
+        
     </div>
+
+    
   )
 }
 
